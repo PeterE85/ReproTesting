027d18235e3b21dce8ea66042a887b48771349cf
Pedro Pinero <penriques1985@gmail.com>
2022-02-24 01:11:41 -0500

    HTML and CSS done

--- main/main.js
+++ main/main.js
@@ -1,1 +1,48 @@
-console.log("helloooo");
+const songList = [
+    {
+        title: "Acoustic Breenze",
+        file: "download.mp3",
+        cover: "gato.jpg"
+    },
+    {
+        title: "A new Beginning",
+        file: "download(1).mp3",
+        cover: "gato2.jpg"
+    },
+    {
+        title: "Creative Minds",
+        file: "download(2).mp3",
+        cover: "gato3.jpg"
+    }
+];
+
+//Capturar elementos del DOM
+const songs = document.getElementById('songs');
+
+//Mostrar listado de canciones
+function loadSongs() {
+    songList.forEach(song => {
+        //console.log(song);
+        //crear li
+        const li = document.createElement("li");
+
+        //crear a
+        const link = document.createElement("a");
+
+        //hidratar a
+        link.textContent = song.title;
+
+        //añadir a li
+        li.appendChild(link);
+
+        
+        //añadir li a ul
+        songs.appendChild(li);
+    });
+
+
+}
+
+//GO
+loadSongs();
+
