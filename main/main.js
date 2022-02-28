4e08d52c72c223c5e27b32f13098e2c7eb181206
Pedro Pinero <penriques1985@gmail.com>
2022-02-28 00:28:42 -0500

    working on JS

--- main/main.js
+++ main/main.js
@@ -6,22 +6,29 @@ const songList = [
     },
     {
         title: "A new Beginning",
-        file: "download(1).mp3",
+        file: "tema.mp3",
         cover: "gato2.jpg"
     },
     {
         title: "Creative Minds",
-        file: "download(2).mp3",
+        file: "temita.mp3",
         cover: "gato3.jpg"
-    }
+    },    
 ];
+
+//Actual SOng
+let actualSong = null;
+
 
 //Capturar elementos del DOM
 const songs = document.getElementById('songs');
+const audio = document.getElementById('audio');
+const cover = document.getElementById('cover');
+const title = document.getElementById('title');
 
 //Mostrar listado de canciones
 function loadSongs() {
-    songList.forEach(song => {
+    songList.forEach((song, index) => {
         //console.log(song);
         //crear li
         const li = document.createElement("li");
@@ -31,6 +38,10 @@ function loadSongs() {
 
         //hidratar a
         link.textContent = song.title;
+        link.href= "#";
+
+        //escuchar clicks
+        link.addEventListener("click", () => loadSong(index));
 
         //añadir a li
         li.appendChild(link);
@@ -39,9 +50,33 @@ function loadSongs() {
         //añadir li a ul
         songs.appendChild(li);
     });
-
-
 }
+
+//Cargar Cancion seleccionada
+function loadSong(songIndex) {
+    if(songIndex =! actualSong) {
+        actualSong = songIndex;
+        //console.log(actualSong);
+    
+        audio.src = "./audio/" + songList[songIndex].file;
+        audio.play();
+    
+        cover.src = "./img/" + songList[songIndex].cover;    
+    
+        title.innerText = songList[songIndex].title;
+
+        changeActiveClass()
+    }    
+}
+
+//change active class
+function changeActiveClass() {
+    const links = document.querySelectorAll("a")
+    console.log(links);
+}
+
+
+
 
 //GO
 loadSongs();
